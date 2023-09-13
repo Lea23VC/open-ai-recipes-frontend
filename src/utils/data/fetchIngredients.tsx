b03c56c3d558eb5c8f5bb6532ccaca26878b1ba5
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-09-12 22:58:42 -0300

    refactor: add initial api configuration

---
+++ src/utils/data/fetchIngredients.tsx
@@ -0,0 +1,13 @@
+import API_URL from '@/config/api';
+import { IngredientList } from '@/ts/types/ingredients.type';
+import axios from 'axios';
+
+export default async function fetchIngredients() {
+  const ingredientsURL = API_URL + 'api/ingredients';
+  return axios
+    .get<{ data: IngredientList }>(ingredientsURL)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+}
