693fd424ff0db993dc399a4f3e1757abee618783
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-09-13 23:21:07 -0300

    feat: add skeletos at first load

--- src/components/selectors/allIngredientsSelectors.tsx
+++ src/components/selectors/allIngredientsSelectors.tsx
@@ -5,6 +5,7 @@ import IngredientsSelector from './ingredientsSelector';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { useIngredientsContext } from '@/context/IngredientsContext';
+import Skeleton from '@mui/material/Skeleton';
 
 export default function AllIngredientsSelector() {
   const { ingredients } = useFetchIngredients();
@@ -14,37 +15,58 @@ export default function AllIngredientsSelector() {
     <Box>
       <Box className="pb-10">
         <Typography
-          className="font-roboto text-black text-3xl font-bold"
           variant="h3"
+          className="font-roboto text-black text-3xl font-bold"
         >
           Seleccione los ingredientes
         </Typography>
       </Box>
 
       <Grid container spacing={3}>
-        {ingredients?.map((ingredient, index) => (
-          <Grid item key={index} xs={12} lg={6}>
-            <Box className="pb-3">
-              <Typography
-                variant="h4"
-                className="!text-base font-robot text-black font-semibold"
-              >
-                {ingredient.label}
-              </Typography>
-            </Box>
-            <Box className="pb-6">
-              <IngredientsSelector ingredientList={ingredient} />
-            </Box>
-          </Grid>
-        ))}
+        {ingredients && ingredients.length > 1
+          ? ingredients.map((ingredient, index) => (
+              <Grid item key={index} xs={12} lg={6}>
+                <Box className="pb-3">
+                  <Typography
+                    variant="h4"
+                    className="text-base font-robot text-black font-semibold"
+                  >
+                    {ingredient.label}
+                  </Typography>
+                </Box>
+                <Box className="pb-6">
+                  <IngredientsSelector ingredientList={ingredient} />
+                </Box>
+              </Grid>
+            ))
+          : new Array(8).fill(null).map((_, index) => (
+              <Grid item key={index} xs={12} lg={6}>
+                <Box className="pb-3">
+                  <Skeleton
+                    className="rounded"
+                    variant="rectangular"
+                    width={100}
+                    height={20}
+                    animation="wave"
+                  />
+                </Box>
+                <Box className="pb-6">
+                  <Skeleton
+                    className="rounded"
+                    variant="rectangular"
+                    width={'100%'}
+                    height={55}
+                    animation="wave"
+                  />
+                </Box>
+              </Grid>
+            ))}
       </Grid>
 
       <Box>
         <Button
           onClick={() => {
-            dispatch({
-              type: 'RESET',
-            });
+            dispatch({ type: 'RESET' });
           }}
           variant="contained"
           className="bg-purple-500 text-white"
